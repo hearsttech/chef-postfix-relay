cb8b558b0beb29888c05e5d79036e7e0d91a1a69
Andrew Coulton <andrew@ingenerator.com>
2014-02-02 00:07:43 +0000

    Implement installing as a remote relay server

---
+++ recipes/install_remote_relay.rb
@@ -0,0 +1,33 @@
+#
+# Installs postfix configured to relay mail to a remote host
+#
+# Author::  Andrew Coulton (<andrew@ingenerator.com>)
+# Cookbook Name:: postfix-relay
+# Recipe:: install_local_dump
+#
+# Copyright 2013-14, inGenerator Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Set postfix access credentials
+node.default['postfix']['main']['relayhost']           = node['postfix_relay']['live_email']['relayhost']
+node.default['postfix']['sasl']['smtp_sasl_passwd']    = node['postfix_relay']['live_email']['smtp_sasl_user_name']
+node.default['postfix']['sasl']['smtp_sasl_user_name'] = node['postfix_relay']['live_email']['smtp_sasl_passwd']
+
+# Ensure that outgoing mail without a domain (eg from local users) is in the correct domain
+node.default['postfix']['main']['myhostname']          = node['postfix_relay']['email_domain']
+# Ensure that mail to addresses in the email_domain is sent externally
+node.default['postfix']['main']['mydestination']       = "#{node['hostname']}, localhost.localdomain, localhost"
+
+include_recipe "postfix::default"
---
+++ spec/install_remote_relay_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'postfix-relay::install_remote_relay' do
+
+  let (:chef_run) do
+    ChefSpec::Runner.new do | node |
+      node.set['postfix_relay']['email_domain']     = 'mysite.com'
+      node.set['postfix_relay']['live_email']['relayhost']           = 'smtp.mydomain.com'
+      node.set['postfix_relay']['live_email']['smtp_sasl_user_name'] = 'me@mydomain.com'
+      node.set['postfix_relay']['live_email']['smtp_sasl_passwd']    = 'password'
+    end.converge(described_recipe)
+  end
+
+  it "sets the postfix hostname to the email_domain" do
+    chef_run.should render_file('/etc/postfix/main.cf').with_content(
+      /^myhostname = mysite.com/m
+    )
+  end
+  
+  it "does not include the email_domain in the postfix mydestination config" do
+    # chefspec is the hostname from Fauxhai
+    chef_run.should render_file('/etc/postfix/main.cf').with_content(
+      /^mydestination = chefspec, localhost.localdomain, localhost$/m
+    )    
+  end
+
+  it "installs postfix with postfix::default" do
+    chef_run.should include_recipe("postfix::default")
+  end
+
+  it "does not include the fs_mail transport in master.cf" do
+    chef_run.should render_file('/etc/postfix/master.cf').with_content(
+      /^(?!.*fs_mail)/m
+    )
+  end
+
+  it "sets the postfix relay configuration to use the configured remote host with credentials" do
+    expect(chef_run.node['postfix']['main']['relayhost']).to eq('smtp.mydomain.com')
+    expect(chef_run.node['postfix']['sasl']['smtp_sasl_passwd']).to eq('me@mydomain.com')
+    expect(chef_run.node['postfix']['sasl']['smtp_sasl_user_name']).to eq('password')
+  end
+
+end
