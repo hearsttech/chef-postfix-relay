0c3cedc946a91d2bdccd05a14199b37c9f27ec8d
Andrew Coulton <andrew@ingenerator.com>
2014-03-11 08:44:03 +0000

    Add sasl_password_file attr for 3.1.x postfix cookbook
    Since opscode-cookbooks/postfix@14fc7d79191c95538bfc2208ceaea028e7348397 the postfix
    cookbook adds a new postfix.sasl_password_file attribute that is only set if 
    smtp_sasl_auth_enable is set to yes at the time the postfix cookbook attributes are
    parsed - so the definition has to be copied into our attributes.

--- attributes/postfix.rb
+++ attributes/postfix.rb
@@ -25,7 +25,10 @@ default['postfix']['main']['smtp_use_tls']                 = 'yes'
 default['postfix']['main']['smtp_tls_security_level']      = 'encrypt'
 default['postfix']['main']['smtp_tls_note_starttls_offer'] = 'yes'
 default['postfix']['main']['smtp_tls_CAfile']              = '/etc/ssl/certs/ca-certificates.crt'
-default['postfix']['main']['smtp_sasl_password_maps']      = "hash:#{node['postfix']['conf_dir']}/sasl_passwd"
+default['postfix']['sasl_password_file']                   = "#{node['postfix']['conf_dir']}/sasl_passwd"
+default['postfix']['main']['smtp_sasl_password_maps']      = "hash:#{node['postfix']['sasl_password_file']}"
 default['postfix']['main']['smtp_sasl_security_options']   = 'noanonymous'
 default['postfix']['main']['smtp_sasl_mechanism_filter']   = 'login'
 
+ 
+  
