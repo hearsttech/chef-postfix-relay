hearsttech/chef-postfix-relay

commit 5cda70dcaa5550fbb97c56b6908ec8bf724743b6

Update default fs_mail user detection for vagrant 2
Vagrant 2 doesn't define the 'vagrant' node attribute key, so detect
the default fs_mail user based on checking the defined user list.
We could just use postfix, but that would make it harder to manage the 
dump file from a Behat context.

attributes/default.rb
@@ -33,5 +33,9 @@ default['postfix_relay']['allow_live_email']  = false
 
 # Configuration for the mail dump. The transport will be included in postfix configuration if active is true
 default['postfix_relay']['fs_mail']['active'] = false
-default['postfix_relay']['fs_mail']['user']   = (node['vagrant'] ? "vagrant" : "ubuntu")
 default['postfix_relay']['fs_mail']['file']   = "/tmp/outgoing_mail.dump"
+if node['etc']['passwd']['vagrant']
+  default['postfix_relay']['fs_mail']['user']   = 'vagrant'
+else
+  default['postfix_relay']['fs_mail']['user']   = 'ubuntu'
+end

commit b3c90c0b74b0e872c1feedea440c9de6b50f413a

Fix build failure introduced by 5cda70dcaa
5cda70dcaa added conditional default of the fs_mail user based on
whether the vagrant or ubuntu user was set, but node.etc.passwd is not
defined inside chefspec so this causes a build failure.

attributes/default.rb
@@ -34,7 +34,7 @@ default['postfix_relay']['allow_live_email']  = false
 # Configuration for the mail dump. The transport will be included in postfix configuration if active is true
 default['postfix_relay']['fs_mail']['active'] = false
 default['postfix_relay']['fs_mail']['file']   = "/tmp/outgoing_mail.dump"
-if node['etc']['passwd']['vagrant']
+if (node['etc'] && node['etc']['passwd'] && node['etc']['passwd']['vagrant'])
   default['postfix_relay']['fs_mail']['user']   = 'vagrant'
 else
   default['postfix_relay']['fs_mail']['user']   = 'ubuntu'